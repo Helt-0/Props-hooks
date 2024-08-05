1724331e329be1236d6236846bc15e1cd7fb3d07
lucas silva <lucas.vasconcelosop@hotmail.com>
2024-08-05 14:23:15 -0300

    :sparkles: feat: Botão de subtrair no contador e Botões de marcar a tarefa como pendente ou inconcluída

--- src/components/Contador/Contador.tsx
+++ src/components/Contador/Contador.tsx
@@ -14,13 +14,20 @@ const [valor, setValor] = useState(0);
     setValor(valor+1);
 
   }
+
+  function subtrairMaisUm(){
+
+    setValor(valor-1);
+
+  }
  
   console.log(valor);
 
   return (
     <div className="container">
         <p>O valor é: {valor}</p>
-        <button onClick={somarMaisUm}>Adicionar+1</button>
+        <button onClick={somarMaisUm}>Somar 1</button>
+        <button onClick={subtrairMaisUm}>Subtrair 1</button>
     </div>
   )
 }
--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -2,34 +2,77 @@ import { useEffect, useState } from 'react';
 import './Task.css'
 
 function Task() {
-  
+
   // Variaveis de estado
   const [finalizada, setFinalizada] = useState<boolean>(false);
-  const [tarefa, setTarefa] = useState<string>("Tarefa Pendente");
+  const [tarefa, setTarefa] = useState<string>("");
+
+  const [incompleta, setIncompleta] = useState<boolean>(false);
 
 
   // Função de efeito colateral
   useEffect(() => {
 
-    if(finalizada === true){    // === Idêntico => verificar o conteúdo e o tipo da info.
+    if (finalizada === true) {    // === Idêntico => verificar o conteúdo e o tipo da info.
 
       setTarefa("Tarefa Concluída");
 
     }
 
+    else if (finalizada === false && incompleta === false) { setTarefa("Tarefa Pendente") }
+
   }, [finalizada])
 
-  function finalizaTarefa(){
+  useEffect(() => {
+
+    if (incompleta === true && finalizada === false) {
+
+      setTarefa("Tarefa Incompleta")
+    }
+
+    else if (incompleta === false && finalizada === true) {
+
+      setTarefa("Tarefa Concluída")
+
+    }
+
+    else if (incompleta === false  && finalizada === false) { 
+      
+      setTarefa("Tarefa Pendente") 
+    
+    }
+
+  }, [incompleta])
+
+
+  function finalizaTarefa() {
 
     setFinalizada(true)
+    setIncompleta(false)
 
   }
-  
+
+  function tarefaIncompleta() {
+
+    setIncompleta(true)
+    setFinalizada(false)
+
+  }
+
+function tarefaPendente(){
+
+  setIncompleta(false)
+  setFinalizada(false)
+
+}
+
   return (
     <div className="container">
-        <h1>Tarefa</h1>
-        <h3>Status da Tarefa: {tarefa}</h3>
-        <button onClick={finalizaTarefa}>Conclua a Tarefa</button>
+      <h1>Tarefa</h1>
+      <h3>Status da Tarefa: {tarefa}</h3>
+      <button onClick={finalizaTarefa}>Concluir a Tarefa</button>
+      <button onClick={tarefaIncompleta}>Tarefa Incompleta</button>
+      <button onClick={tarefaPendente}>Tarefa Pendente</button>
     </div>
   )
 }
